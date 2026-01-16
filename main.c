f278514f3e9479bccde2cedd19e1d10a18647cf6
keks137 <keks137@proton.me>
2026-01-16 20:11:02 +0100

    exec from code instead

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-#define ELF3_HDR_SIZE 0x78 // ELF header + 1 program header
+#define ELF643_HDR_SIZE 0x78 // ELF header + 1 program header
 #define VIRTUAL_OFFSET 0x400000
 
-static void write_elf3(FILE *f, uint8_t *code, size_t codelen, uint64_t entry)
+#define ALIGN(x, a) (((x) + (a) - 1) & ~((a) - 1))
+
+static void write_elf643(FILE *f, uint8_t *code, size_t codelen, uint64_t entry)
 {
-	uint8_t hdr[ELF3_HDR_SIZE] = { 0 };
+	uint8_t hdr[ELF643_HDR_SIZE] = { 0 };
 
 	memcpy(hdr, "\x7f"
 		    "ELF",
@@ -19,7 +22,7 @@ static void write_elf3(FILE *f, uint8_t *code, size_t codelen, uint64_t entry)
 	*(uint16_t *)(hdr + 0x10) = 2; // e_type = ET_EXEC
 	*(uint16_t *)(hdr + 0x12) = 0x3e; // e_machine = EM_X86_64
 	*(uint32_t *)(hdr + 0x14) = 1; // e_version
-	*(uint64_t *)(hdr + 0x18) = entry; // e_entry
+	*(uint64_t *)(hdr + 0x18) = VIRTUAL_OFFSET + ELF643_HDR_SIZE + entry; // e_entry
 	*(uint64_t *)(hdr + 0x20) = 0x40; // e_phoff
 	*(uint16_t *)(hdr + 0x34) = 0x40; // e_ehsize
 	*(uint16_t *)(hdr + 0x36) = 0x38; // e_phentsize
@@ -30,19 +33,59 @@ static void write_elf3(FILE *f, uint8_t *code, size_t codelen, uint64_t entry)
 	*(uint64_t *)(hdr + 0x48) = 0; // p_offset = 0
 	*(uint64_t *)(hdr + 0x50) = VIRTUAL_OFFSET; // p_vaddr
 	*(uint64_t *)(hdr + 0x58) = 0; // p_paddr
-	*(uint64_t *)(hdr + 0x60) = ELF3_HDR_SIZE + codelen; // p_filesz
-	*(uint64_t *)(hdr + 0x68) = ELF3_HDR_SIZE + codelen; // p_memsz
+	*(uint64_t *)(hdr + 0x60) = ELF643_HDR_SIZE + codelen; // p_filesz
+	*(uint64_t *)(hdr + 0x68) = ELF643_HDR_SIZE + codelen; // p_memsz
 	*(uint64_t *)(hdr + 0x70) = 0x1000; // p_align
 
-	fwrite(hdr, 1, ELF3_HDR_SIZE, f);
+	fwrite(hdr, 1, ELF643_HDR_SIZE, f);
 	fwrite(code, 1, codelen, f);
 }
 
-static uint8_t code[] = {
-	0x48, 0xC7, 0xC0, 0x3C, 0x00, 0x00, 0x00, 0x48, 0xC7, 0xC7, 0x2A, 0x00, 0x00, 0x00, 0x0F, 0x05
-};
+typedef struct {
+	uint8_t *data;
+	size_t cap;
+	size_t lvl;
+} BinArray;
+
+static void binarr_push_byte(BinArray *arr, uint8_t data)
+{
+	arr->data[arr->lvl] = data;
+	arr->lvl++;
+}
+
+static void binarr_push(BinArray *arr, const void *data, size_t len)
+{
+	memcpy(&arr->data[arr->lvl], data, len);
+	arr->lvl += len;
+}
+
+static void movm64imm32(BinArray *arr, uint8_t reg, uint32_t imm)
+{
+	const uint8_t rexw_prefix = 0x48;
+	binarr_push(arr, &rexw_prefix, sizeof(rexw_prefix));
+	const uint8_t opcode = 0xC7;
+	binarr_push(arr, &opcode, sizeof(opcode));
+	binarr_push(arr, &reg, sizeof(reg));
+	binarr_push(arr, &imm, sizeof(imm));
+}
+
+static void syscall(BinArray *arr)
+{
+	binarr_push_byte(arr, 0x0F);
+	binarr_push_byte(arr, 0x05);
+}
 
 int main()
 {
-	write_elf3(stdout, code, sizeof(code), VIRTUAL_OFFSET + ELF3_HDR_SIZE);
+	BinArray code = { 0 };
+	{
+		static uint8_t data[512];
+		code.data = &data[0];
+	}
+
+	movm64imm32(&code, 0xC0, 60);
+	movm64imm32(&code, 0xC7, 69);
+	syscall(&code);
+
+	write_elf643(stdout, code.data, code.lvl, 0);
 }
